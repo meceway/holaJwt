9a15dc05aa43a4bd400616d53279c29595ac0748
meceway <aikur@live.com>
2022-06-19 00:48:34 +0800

    更新检查逻辑

--- src/KEY.php
+++ src/KEY.php
@@ -13,11 +13,13 @@
         {
             list($key, $alg) = $keyAndAlg;
 
-            if (!is_string($key)) throw new TypeError('key must be string');
-            if (!is_string($alg)) throw new TypeError('alg must be string');
-
+            //  检查秘钥以及算法是否为空
             if (empty($key)) throw new InvalidArgumentException('key is empty');
             if (empty($alg)) throw new InvalidArgumentException('alg is empty');
+
+            //  检查秘钥和算法是否合法
+            if (!is_string($key)) throw new TypeError('key must be string');
+            if (!is_string($alg)) throw new TypeError('alg must be string');
 
             $this->key = $key;
             $this->alg = $alg;
